c0fbb072e09c53a18590d8379aa84ec30a7d2e44
Sergiy Shyrkov <sergiy.shyrkov@nortal.com>
2020-03-22 12:06:09 +0100

    Playing with headers for axios

--- src/helpers/http.tsx
+++ src/helpers/http.tsx
@@ -34,7 +34,7 @@ const getErrorCallback = (callback? : (error: any) => void) => {
 };
 
 const getRequestConfig = (auth?: AuthOptions) => {
-    return auth ? {auth: {username: auth.username, password: auth.password}, headers: {'Content-Type': 'application/json'}} : {headers: {'Content-Type': 'application/json'}};
+    return auth ? {auth: {username: auth.username, password: auth.password}, headers: {'Content-Type': 'application/json', 'X-Requested-With': 'Corona-Control'}} : {headers: {'Content-Type': 'application/json', 'X-Requested-With': 'Corona-Control'}};
 };
 
 export {get, post};

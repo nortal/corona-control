5bf5d9ce9d91e89b0e8a2c9aa05a18bf7e9eec5c
Dmitri Gristsenko <dmitri.gristsenko@nortal.com>
2020-03-22 15:08:30 +0100

    Added Resource total blocks and examples of their use

--- src/components/views/Dashboard.tsx
+++ src/components/views/Dashboard.tsx
@@ -4,6 +4,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper } from '@material-ui/core';
 import ResourceStatisticsCard, {ResourceStatisticsCardProps} from "../ResourceStatisticsCard";
 import {StockStatus} from "../../api/model/ResourceStatusPayload";
+import ResourceTotalCard, {ResourceTotalCardProps} from "../ResourceTotalCard";
+
+import ICU from "../../assets/ICU.png";
+import people from "../../assets/people.png";
+import bed from "../../assets/Bed.png";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,6 +17,7 @@ const useStyles = makeStyles(theme => ({
     },
     paper: {
         padding: theme.spacing(2),
+        backgroundColor: "#E7E6E6",
         display: 'flex',
         overflow: 'auto',
         flexDirection: 'row',
@@ -37,21 +43,27 @@ const sampleStatistics = [{label: "Chirurgische Masken", nrOk: 50, nrLow: 150, n
             {"name": "St. Vinzenz-Krankenhaus", contact: " 0211 8100", stockStatus: StockStatus.OUT_OF_STOCK}
         ]}];
 
+const sampleResourceTotals = [
+    {label: "COVID-19-Patienten", iconSrc: people, numberTotal: 234},
+    {label: "In Intensivstationen", iconSrc: ICU, numberTotal: 13},
+    {label: "ICU lvl 3", iconSrc: ICU, numberTotal: 2},
+    {label: "Freie Betten", iconSrc: bed, numberTotal: 1203},
+    {label: "Ventilatoren", iconSrc: bed, numberTotal: 457},
+    ];
+
 const Dashboard = () => {
     const classes = useStyles();
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
     const [resourceStatistics, setResourceStatistics] = useState<ResourceStatisticsCardProps[]>(sampleStatistics);
+    const [resourceTotals, setResourceTotals] = useState<ResourceTotalCardProps[]>(sampleResourceTotals);
+
 
 
     return (
         <Grid container spacing={3}>
-            <Grid item xs={12} md={8} lg={9}>
+            <Grid item xs={12}>
                 <Paper className={fixedHeightPaper}>
-
-                </Paper>
-            </Grid>
-            <Grid item xs={12} md={4} lg={3}>
-                <Paper className={fixedHeightPaper}>
+                    {resourceTotals.map((resourceTotal) => <ResourceTotalCard {...resourceTotal}/>)}
                 </Paper>
             </Grid>
             <Grid item xs={12}>
